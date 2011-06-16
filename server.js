3a86d07b887b579ee6e10e397cf94c73a0aadff2
Nicolas Chambrier <naholyr@gmail.com>
2011-06-16 13:33:22 +0200

    demo filesystem

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('coucou gamin');
+var fs = require('fs');
+
+function onReadFile (filename) {
+  return function (err, content) {
+    console.log('Lecture "%s": %s', filename, err ? ('Erreur = ' + (err.message || err)) : content);
+  }
+}
+
+fs.readFile('fail', onReadFile('fail'));
+fs.readFile('README', onReadFile('README'));
