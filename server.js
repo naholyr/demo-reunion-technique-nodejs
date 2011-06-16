a1510bdd567f8d241385e5d61c7074f2174b85ca
Nicolas Chambrier <naholyr@gmail.com>
2011-06-16 16:49:22 +0200

    Added connect (multiple dependencies)

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ console.log('Méthodes', Object.keys(convert));
 console.log('1337 en base 16 = %s', convert.decToHex(1337));
 console.log('1337 en base 36 = %s', convert.decTo36(1337));
 console.log('1337 en base 62 = %s', convert.decTo62(1337));
+
+console.log("Connect version: %s", require('connect').version);
