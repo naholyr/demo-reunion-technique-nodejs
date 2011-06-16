8286133c73d9a30432219adb5755c8fd3f225833
Nicolas Chambrier <naholyr@gmail.com>
2011-06-16 13:44:45 +0200

    async.js

--- server.js
+++ server.js
@@ -1,6 +1,1 @@
-var convert = require('base-converter');
-
-console.log('Méthodes', Object.keys(convert));
-console.log('1337 en base 16 = %s', convert.decToHex(1337));
-console.log('1337 en base 36 = %s', convert.decTo36(1337));
-console.log('1337 en base 62 = %s', convert.decTo62(1337));
+console.log('Goto http://naholyr.fr/2011/06/bonnes-pratiques-asynchrone-javascript-nodejs/');
